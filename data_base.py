f6d901b2ba0ce2ae42d6d23b74fce37f59cb3802
Tommy <2764393408@qq.com>
2025-11-24 19:31:30 +0800

    添加redis分布式锁功能

--- data_base.py
+++ data_base.py
@@ -1,6 +1,8 @@
 import aiomysql
 import asyncio
 import redis
+import uuid
+import time
 from util import sendEmail, logger
 
 
@@ -45,17 +47,16 @@ class MysqlManager(object):
             await connObject.conn.rollback()
             logger.error(e)
 
-
-@staticmethod
-async def updateMysqlUser(connObject: object, sql: str, param: tuple) -> tuple:
-    try:
-        await connObject.cursor.execute(sql, param)
-        await connObject.conn.commit()
-        return True
-    except aiomysql.MySQLError as e:
-        await connObject.conn.rollback()
-        logger.error(e)
-        return False
+    @staticmethod
+    async def updateMysqlUser(connObject: object, sql: str, param: tuple) -> tuple:
+        try:
+            await connObject.cursor.execute(sql, param)
+            await connObject.conn.commit()
+            return True
+        except aiomysql.MySQLError as e:
+            await connObject.conn.rollback()
+            logger.error(e)
+            return False
 
 
 async def registerMysqlUserSendEmail(
@@ -78,7 +79,7 @@ class RedisManager(object):
     async def init(cls, db: int) -> object:
         instance = cls()
         pool = redis.ConnectionPool(
-            host="localhost", port=6379, db=db, password=123456, decode_responses=True
+            host="localhost", port=6379, db=db, password="123456", decode_responses=True
         )
         redisObject = redis.Redis(connection_pool=pool)
         instance.object = redisObject
@@ -107,3 +108,87 @@ class RedisManager(object):
             logger.debug(f"Redis {key} 已删除")
         except Exception as e:
             raise RuntimeError(f"删除失败{e}")
+
+    @staticmethod
+    async def acuire_lock(
+        connObject: object, lock_name: str, acquire_out_time=10, lock_time_out=30
+    ):
+        """获取分布式锁"""
+
+        identifier = str(uuid.uuid4())
+        lock_name = f"lock:{lock_name}"
+        end = time.time() + acquire_out_time
+
+        while time.time() < end:
+            # 这个循环给用户良好的体验,一直拿不到所就返回错误,页面不会一直转圈圈等待
+            if connObject.object.set(lock_name, identifier, ex=lock_time_out):
+                # 过期时间和创建锁写在一个命令里保证原子性防止无过期时间出现死锁,加上续期防止业务超时
+                renew_exp = asyncio.create_task(
+                    RedisManager.addExpTime(
+                        connObject, lock_name, identifier, lock_time_out
+                    )
+                )
+                return identifier, renew_exp
+            asyncio.sleep(0.001)
+        return None, None
+
+    @staticmethod
+    async def release_lock(connObject: object, lock_name: str, identifier: str):
+        """释放锁"""
+
+        # 编写LUA脚本,防止误删别人的锁
+        unlock_script = b"""
+            if redis.call("get",KEYS[1])==ARGV[1] then
+                return redis.call("del",KEYS[1])
+            else
+                return 0
+            end
+        """
+        lock_name = f"lock:{lock_name}"
+        unlock = connObject.object.register_script(unlock_script)
+        result = unlock(keys=[lock_name], args=[identifier])
+        if result:
+            logger.info("释放锁成功")
+            return True
+        else:
+            return False
+
+    @staticmethod
+    async def addExpTime(
+        connObject: object, lock_name: str, indentifier: str, lock_time_out: int
+    ):
+        """给锁延期"""
+        # 过期的1/3时间段检查是否还持有锁
+        renew_interval = lock_time_out * 2 // 3
+
+        while True:
+            await asyncio.sleep(renew_interval)
+
+            # 编写LUA脚本判断当前用户是否还持有锁
+            renew_script = b"""
+                if redis.call("get",KEYS[1])==ARGV[1] then
+                    return redis.call("expire",KEYS[1],ARGV[2])
+                else
+                    return 0
+                end
+            """
+            lock_name = f"lock:{lock_name}"
+            re_Expire = connObject.register_script(renew_script)
+            result = re_Expire(keys=[lock_name], args=[indentifier, lock_time_out])
+            if not result:
+                break
+
+    @staticmethod
+    async def addExpTimeCancel(
+        connObject: object, lock_name: str, identifier: str, renew_exp
+    ):
+        """关闭续期并释放锁"""
+        try:
+            if renew_exp:
+                renew_exp.cancel()
+                logger.info("关闭续期")
+        except asyncio.CancelledError as e:
+            logger.warning(f"关闭续期失败原因:{e}")
+
+        finally:
+            await RedisManager.release_lock(connObject, lock_name, identifier)
