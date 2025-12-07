e6fda758f8d7f1ec9f137200e782eb22480bbd97
Tommy <2764393408@qq.com>
2025-12-07 22:34:11 +0800

    orderService的输入输出模型

---
+++ orderService/schema.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+from typing import Dict, Any
+
+
+class CreateOrder(BaseModel):
+    idenID: str
+    userID: str
+    goodsInfo: Dict[str, Any]
+
+
+class RushPurchaseInput(BaseModel):
+    goodsname: str
+    goodsID: int
+    userID: int
+    number: int
